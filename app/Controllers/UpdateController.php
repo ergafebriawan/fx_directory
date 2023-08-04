e9423dd782740b34a7edd80a982cf1dc7ba21040
ergafebriawan <ergafebriawan@gmail.com>
2023-08-04 08:53:25 +0700

    update page v1

--- app/Controllers/UpdateController.php
+++ app/Controllers/UpdateController.php
@@ -1,29 +1,67 @@
 <?php
-/*
- * HomeController only for controller sample
- * @hilmanrdn 18-01-2017
- */
-
 namespace App\Controllers;
+
+use Illuminate\Support\Facades\Redirect;
 
 class UpdateController extends BaseController
 {
     public function index($request, $response){
-        return $this->c->view->render($response, 'update.html');
+        $query = $this->c->db->query('SELECT * FROM fx_update ORDER BY id ASC');
+        $data = $query->fetch_all(MYSQLI_ASSOC);
+
+        return $this->c->view->render($response, 'update.html', ['data' => $data]);
+    }
+
+    public function detail($request, $response, array $args){
+        $data = $this->c->db->query('SELECT * FROM fx_update WHERE id='.$args["id"].' LIMIT 1');
+        $data_set = $data->fetch_assoc();
+        return $this->c->view->render($response, 'update-detail.html', ['data' => $data_set]);
+    }
+
+    public function delete($request, $response, array $args){
+        $this->c->db->query('DELETE FROM fx_update WHERE id='.$args['id']);
+        return $response->withRedirect('/update');
     }
 
     public function add($request, $response){
-        if($request->isMethod('get')){
+        if($request->isPost()){
             $title = $_POST['in_title'];
             $content = $_POST['in_content'];
 
             $data = [
-                "judul" => "hehe",
-                "konten" => "hehehehe"
+                "judul" => $title,
+                "konten" => $content
             ];
-            var_dump($data);
+            $exec = $this->c->db->query('INSERT INTO fx_update (id, title, content, created_at) VALUES (NULL, "'.$title.'", "'.$content.'", CURRENT_TIMESTAMP)');
+
+            if($exec){
+                return $response->withRedirect('/update');
+            }else{
+                return $response->withRedirect('/update-add');
+            }
         }else{
             return $this->c->view->render($response, 'update-add.html');
+        }
+    }
+
+    public function edit($request, $response, array $args){
+        if($request->isPost()){
+            $id_update = $_POST['in_id'];
+            $title = $_POST['in_title'];
+            $content = $_POST['in_content'];
+
+            $exec = $this->c->db->query('UPDATE fx_update SET title="'.$title.'", content="'.$content.'" WHERE id='.$id_update);
+
+            if($exec){
+                return $response->withRedirect('/update');
+            }else{
+                return $response->withRedirect('/update-edit/'.$id_update);
+            }
+        }else{
+            $data = $this->c->db->query('SELECT * FROM fx_update WHERE id='.$args["id"].' LIMIT 1');
+            $data_set = $data->fetch_assoc();
+            
+            return $this->c->view->render($response, 'update-edit.html', ['data' => $data_set]);
         }
     }
 
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,6 @@ $app->any('/library-upload', '\App\Controllers\LibraryController:add');
 
 $app->get('/update', '\App\Controllers\UpdateController:index');
 $app->any('/update-add', '\App\Controllers\UpdateController:add');
+$app->get('/update-detail/{id}', '\App\Controllers\UpdateController:detail');
+$app->get('/update-delete/{id}', '\App\Controllers\UpdateController:delete');
+$app->any('/update-edit/{id}', '\App\Controllers\UpdateController:edit');
